64d0ba8d86a81f3c60e04e80126c449e589d3e81
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-01 15:01:02 +0300

    fix: fixing some bug.

--- src/main/java/com/project/gouteko/controller/ProductController.java
+++ src/main/java/com/project/gouteko/controller/ProductController.java
@@ -1,13 +1,14 @@
 package com.project.gouteko.controller;
 
 import com.project.gouteko.model.Product;
+import com.project.gouteko.model.User;
 import com.project.gouteko.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,22 +17,31 @@ import java.util.UUID;
 public class ProductController {
     private final ProductService productService;
 
+    @GetMapping("/")
+    public List<Product> getAll(){
+        return productService.findAll();
+    }
     @GetMapping("/{productName}")
     public Product getProductName(@PathVariable String productName){
         return productService.getProductByName(productName);
     }
+    @PostMapping("/create")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product newProduct = productService.create(product);
+        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+    }
 
-    @PutMapping("/crupdate/{id}")
-    public ResponseEntity<Product> crupdateProduct(@PathVariable UUID id, @RequestBody Product product){
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable UUID id, @RequestBody Product product){
         try {
-            productService.createOrUpdateProduct(id,product);
+            productService.updateProduct(id,product);
             return new ResponseEntity<>(product, HttpStatus.OK);
         }
         catch (RuntimeException e){
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
-    @DeleteMapping("/id")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletProduct(@PathVariable UUID id){
         try {
             productService.deleteProduct(id);
--- src/main/java/com/project/gouteko/service/ProductService.java
+++ src/main/java/com/project/gouteko/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.project.gouteko.service;
 
 import com.project.gouteko.model.Product;
+import com.project.gouteko.model.User;
 import com.project.gouteko.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,20 +23,20 @@ public class ProductService {
         }
         else throw new RuntimeException("Product not found with id: \" "+ productName );
     }
-    public Product createOrUpdateProduct(UUID id,Product crupdateProduct){
+    public Product updateProduct(UUID id,Product crupdateProduct){
         Optional<Product> existingProduct = productRepository.findById(id);
 
         if(existingProduct.isPresent()){
             Product product = existingProduct.get();
             product.setName(crupdateProduct.getName());
-            product.setDescription(crupdateProduct.getDescription());
-            product.setProductImage(crupdateProduct.getProductImage());
             product.setPrice(crupdateProduct.getPrice());
+            product.setDescription(crupdateProduct.getDescription());
             product.setAvailableQuantity(crupdateProduct.getAvailableQuantity());
+            product.setCategory(crupdateProduct.getCategory());
 
             return productRepository.save(product);
         }
-        else return productRepository.save(crupdateProduct);
+        else throw new RuntimeException("Product id not found" + id);
     }
     public void deleteProduct(UUID id){
         Optional<Product> productId = productRepository.findById(id);
@@ -44,5 +46,13 @@ public class ProductService {
         else {
             productRepository.deleteById(id);
         }
+    }
+    public Product create(Product product){
+        return  productRepository.save(product);
+    }
+
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
     }
 }
