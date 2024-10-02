5da7bb57565dfac596bacd30fc3d8600697bfc75
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-02 09:10:29 +0300

    fix : fixing bug in user mapper

--- src/main/java/com/project/gouteko/controller/mapper/UserMapper.java
+++ src/main/java/com/project/gouteko/controller/mapper/UserMapper.java
@@ -2,10 +2,11 @@ package com.project.gouteko.controller.mapper;
 
 import com.project.gouteko.DTO.UserDTO;
 import com.project.gouteko.model.User;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Base64;
-
+@Component
 public class UserMapper {
 
         public static User toDomain(UserDTO userDTO) throws Exception {
@@ -13,6 +14,8 @@ public class UserMapper {
             domainUser.setFirstName(userDTO.getFirstName());
             domainUser.setLastName(userDTO.getLastName());
             domainUser.setEmail(userDTO.getEmail());
+            domainUser.setPhoneNumber(userDTO.getAddress());
+            domainUser.setAddress(userDTO.getAddress());
 
             MultipartFile imageFile = userDTO.getImage();
             if (imageFile != null && !imageFile.isEmpty()) {
@@ -25,12 +28,13 @@ public class UserMapper {
         }
 
 
-        public static UserDTO toView(User user) {
+        public UserDTO toView(User user) {
             UserDTO userDTO = new UserDTO();
             userDTO.setFirstName(user.getFirstName());
             userDTO.setLastName(user.getLastName());
             userDTO.setEmail(user.getEmail());
-
+            userDTO.setPhone(user.getPhoneNumber());
+            userDTO.setAddress(user.getAddress());
 
             String base64Image = user.getImage();
             userDTO.setImageBase64(base64Image);
