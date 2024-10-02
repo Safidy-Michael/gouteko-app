647d3cfd61f0ae65f06ae9143851b8317e05c954
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-02 09:11:51 +0300

    feat: update user DTO

--- src/main/java/com/project/gouteko/DTO/UserDTO.java
+++ src/main/java/com/project/gouteko/DTO/UserDTO.java
@@ -1,4 +1,19 @@
 package com.project.gouteko.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private MultipartFile image;
+    private String imageBase64;
 }
