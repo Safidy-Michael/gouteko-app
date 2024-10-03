d4bae3cdf3260f5b989b52c019a12c44016070de
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-03 11:14:31 +0300

    chore: updating userDTO

--- src/main/java/com/project/gouteko/DTO/UserDTO.java
+++ src/main/java/com/project/gouteko/DTO/UserDTO.java
@@ -5,14 +5,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
+    private UUID id;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
+    private String password;
     private String address;
     private MultipartFile image;
     private String imageBase64;
