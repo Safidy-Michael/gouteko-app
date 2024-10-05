a4f84955b7272126a6f169acb18cde175284c365
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-05 15:21:41 +0300

    feat: Configure CORS for frontend communication with backend

--- src/main/java/com/project/gouteko/config/WebConfig.java
+++ src/main/java/com/project/gouteko/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.project.gouteko.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permet tous les endpoints
-                .allowedOrigins("http://localhost:3001") // Remplacez par l'origine de votre application React
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
