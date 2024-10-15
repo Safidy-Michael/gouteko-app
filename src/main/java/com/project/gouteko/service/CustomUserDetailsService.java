7dad85e4f00f737aa641561efc402ff203ff3ff5
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-15 11:20:10 +0300

    chore:Refactor Custom User Details Service

--- src/main/java/com/project/gouteko/service/CustomUserDetailsService.java
+++ src/main/java/com/project/gouteko/service/CustomUserDetailsService.java
@@ -2,28 +2,34 @@ package com.project.gouteko.service;
 
 import com.project.gouteko.model.User;
 import com.project.gouteko.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPassword(),
-                user.getRoles() // Ensure this is a valid collection of GrantedAuthority
-        );
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
     }
+
 }
