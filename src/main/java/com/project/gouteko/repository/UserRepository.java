3f0fb244334d628ba7c4bcff72230a513c394d4d
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-15 11:18:16 +0300

    chore: Refactor User Repository

--- src/main/java/com/project/gouteko/repository/UserRepository.java
+++ src/main/java/com/project/gouteko/repository/UserRepository.java
@@ -10,4 +10,6 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User,UUID> {
     Optional<User> findByFirstName(String userName);
+
+    Optional<User> findByEmail(String email);
 }
