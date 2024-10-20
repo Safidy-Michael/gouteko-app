a3381388b1a70da56db2a8be9422b78ada66f0bf
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-20 11:39:46 +0300

    chore: updating login response

--- src/main/java/com/project/gouteko/DTO/LoginResponse.java
+++ src/main/java/com/project/gouteko/DTO/LoginResponse.java
@@ -3,9 +3,12 @@ package com.project.gouteko.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 public class LoginResponse {
+    private UUID id;
     private String email;
     private String token;
 }
